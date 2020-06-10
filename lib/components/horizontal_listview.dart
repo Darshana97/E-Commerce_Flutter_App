a1a83f7093d9e1c4d96f10a65ff81640ad56e0eb
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-10 21:27:30 +0530

    categories

--- lib/components/horizontal_listview.dart
+++ lib/components/horizontal_listview.dart
@@ -4,10 +4,39 @@ class HorizontalList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80.0,
+      height: 100.0,
       child: ListView(
         scrollDirection: Axis.horizontal,
-        children: <Widget>[],
+        children: <Widget>[
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'tshirt',
+          ),
+          Category(
+            image_location: 'images/cats/shoe.png',
+            image_caption: 'shoes',
+          ),
+          Category(
+            image_location: 'images/cats/jeans.png',
+            image_caption: 'jeans',
+          ),
+          Category(
+            image_location: 'images/cats/informal.png',
+            image_caption: 'informal',
+          ),
+          Category(
+            image_location: 'images/cats/formal.png',
+            image_caption: 'formal',
+          ),
+          Category(
+            image_location: 'images/cats/dress.png',
+            image_caption: 'dress',
+          ),
+          Category(
+            image_location: 'images/cats/accessories.png',
+            image_caption: 'accessories',
+          ),
+        ],
       ),
     );
   }
@@ -25,9 +54,17 @@ class Category extends StatelessWidget {
       padding: EdgeInsets.all(2.0),
       child: InkWell(
         onTap: () {},
-        child: ListTile(
-          title: Image.asset(image_location),
-          subtitle: Text(image_caption),
+        child: Container(
+          width: 100.0,
+          child: ListTile(
+            title: Image.asset(
+              image_location,
+              width: 100.0,
+              height: 80.0,
+            ),
+            subtitle: Container(
+                alignment: Alignment.topCenter, child: Text(image_caption)),
+          ),
         ),
       ),
     );
