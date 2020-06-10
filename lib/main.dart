0100a4241c8aa09a1be3df2df852185cf521cb15
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-10 10:57:40 +0530

    create drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomePage(),
@@ -17,18 +17,33 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         title: Text("My Shop"),
         centerTitle: true,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search,color: Colors.white,), onPressed: (){} ),
-          IconButton(icon: Icon(Icons.shopping_cart,color: Colors.white,), onPressed: (){} )
+          IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {})
         ],
       ),
-
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                accountName: Text("Darshana Senevirathna"),
+                accountEmail: Text("dpbrayan1@gmail.com"))
+          ],
+        ),
+      ),
     );
   }
 }
-
-
-
-
