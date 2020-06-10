2cf3266d1af6bee79c49329db0a69b6cb50634ce
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-10 10:25:14 +0530

    add images

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
 
 void main(){
   runApp(MaterialApp(
     home: HomePage(),
   ));
 }
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Shop"),
+      ),
+    );
+  }
+}
+
+
 
 
