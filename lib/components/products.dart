b32b9c8b89c30336ad44bf5c0d4450631c5e5cfe
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-11 21:31:34 +0530

    products

--- lib/components/products.dart
+++ lib/components/products.dart
@@ -26,3 +26,21 @@ class _ProductsState extends State<Products> {
     return Container();
   }
 }
+
+class Single_prod extends StatelessWidget {
+  final prod_name;
+  final prod_picture;
+  final prod_old_picture;
+  final prod_price;
+
+  Single_prod(
+      {this.prod_name,
+      this.prod_picture,
+      this.prod_old_picture,
+      this.prod_price});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
