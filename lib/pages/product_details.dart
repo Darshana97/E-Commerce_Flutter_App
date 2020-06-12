3d555d01a67f7b4f174807bdadd654685165b5c9
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-12 10:33:39 +0530

    add product details

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatefulWidget {
+
+  final product_detail_name;
+  final product_detail_new_price;
+  final product_detail_old_price;
+  final product_detail_picture;
+
+  ProductDetails(
+      {this.product_detail_name, this.product_detail_new_price, this.product_detail_old_price, this.product_detail_picture});
+
   @override
   _ProductDetailsState createState() => _ProductDetailsState();
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +38,14 @@ class _ProductDetailsState extends State<ProductDetails> {
                 color: Colors.white,
               ),
               onPressed: () {})
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 300.0,
+            color: Colors.black,
+          )
         ],
       ),
     );
