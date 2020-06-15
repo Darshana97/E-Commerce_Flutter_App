5c59f9667d776a48f6d0c58047b1efb67684adc0
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-15 09:16:19 +0530

    cart products

--- lib/components/cart_products.dart
+++ lib/components/cart_products.dart
@@ -6,10 +6,33 @@ class Cart_products extends StatefulWidget {
 }
 
 class _Cart_productsState extends State<Cart_products> {
+  var Products_on_the_cart = [
+    {
+      "name": "Blazer",
+      "picture": "images/products/blazer1.jpeg",
+      "price": 85,
+      "size":"M",
+      "color":"Black",
+      "quantity":1
+    },
+    {
+      "name": "Shoes",
+      "picture": "images/products/hills1.jpeg",
+      "price": 50,
+      "size":"7",
+      "color":"Red",
+      "quantity":1
+    }
+
+
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: 3, itemBuilder: (context, index) {
-      return Text("aaa");
-    });
+    return ListView.builder(
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return Text("aaa");
+        });
   }
 }
